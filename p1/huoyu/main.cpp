1c8de655bae2660c455e4f6bccb1cee58d0d10d7
winhuo <1262414870@qq.com>
2019-03-13 03:16:30 +0800

    t

--- p1/huoyu/main.cpp
+++ p1/huoyu/main.cpp
@@ -1,2 +1,19 @@
-hhhh
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> tar;
+        for(int i=0;i<nums.size();++i)
+        {
+            for(int j=i+1;j<nums.size();++j)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    tar.push_back(i);
+                    tar.push_back(j);
+                    return tar;
+                }
+            }
+        }
+        return tar;
+    }
+};
